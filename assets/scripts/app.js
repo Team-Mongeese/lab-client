61fa22c5ec4c65569e8d790b4ebf0f64d2768ed3
jaemcu <cupp.j.e@gmail.com>
2020-01-03 15:27:42 -0500

    Feature: app.js file
    1. Updated app.js file with path to events.js file in books folder
    2. Updated app.js file with addHandlers for events.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,10 +2,11 @@
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
+const bookEvents = require('./book/events')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  bookEvents.addHandlers()
 })
